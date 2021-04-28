c0a200576fa17af44687fa255af65c62269876f8
Ryan Wilson <syndicated.life@gmail.com>
2021-04-28 12:57:58 -0700

    latest release with deps

--- FFXIVAPP.Plugin.Widgets/Properties/Settings.cs
+++ FFXIVAPP.Plugin.Widgets/Properties/Settings.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Settings.cs" company="SyndicatedLife">
-//   Copyright© 2007 - 2020 Ryan Wilson &amp;lt;syndicated.life@gmail.com&amp;gt; (https://syndicated.life/)
+//   Copyright© 2007 - 2021 Ryan Wilson &amp;lt;syndicated.life@gmail.com&amp;gt; (https://syndicated.life/)
 //   Licensed under the MIT license. See LICENSE.md in the solution root for full license information.
 // </copyright>
 // <summary>
@@ -38,7 +38,7 @@ namespace FFXIVAPP.Plugin.Widgets.Properties {
 
         private static Settings _default;
 
-        public new event PropertyChangedEventHandler PropertyChanged = delegate { };
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
 
         public static Settings Default {
             get {
@@ -647,7 +647,7 @@ namespace FFXIVAPP.Plugin.Widgets.Properties {
             }
         }
 
-        public new void Reset() {
+        public void Reset() {
             this.DefaultSettings();
             foreach (var key in Constants.Settings) {
                 SettingsProperty settingsProperty = Default.Properties[key];
